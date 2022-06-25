2cc79b2e9427fab5983c293d90dccb870e97fb72
amitosm <amitmargalit1@gmail.com>
2022-06-25 19:30:23 +0300

    fixing the path

--- backend/server.js
+++ backend/server.js
@@ -44,7 +44,7 @@ app.use(
     },
   })
 );
-app.use(express.static(path.resolve(__dirname, "./frontend/build")));
+app.use(express.static(path.resolve(__dirname, "../frontend/build")));
 
 
 //seassion initialize 
